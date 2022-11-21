7321104672801a69062f163c4fc006cda51d49cb
Erick Ochieng <erickochieng766@gmail.com>
2022-11-21 16:04:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Erick Ochieng"
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+let shortGreeting = 'Welcome, '+currentUser[0]+"!"
